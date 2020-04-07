913e09dc83efa8793d0126606a792ce7fc6aa195
Jacob Whisler <jwhisler521@gmail.com>
2020-04-07 16:28:09 -0400

    changed unused res params to _

--- routes/api.js
+++ routes/api.js
@@ -21,7 +21,7 @@ router.post("/api/transaction/bulk", ({body}, res) => {
     });
 });
 
-router.get("/api/transaction", (req, res) => {
+router.get("/api/transaction", (_, res) => {
   Transaction.find({}).sort({date: -1})
     .then(dbTransaction => {
       res.json(dbTransaction);
